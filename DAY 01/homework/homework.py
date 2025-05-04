48b4db67af458b5dd56a70141895c262ee1019b8
Wuxela <wuxela7@gmail.com>
2025-05-04 16:55:31 +0400

    commit

---
+++ DAY 01/homework/homework.py
@@ -0,0 +1,17 @@
+name = "leqso"
+age = 15 
+address = "gori"
+height = 170
+weight = 85
+print("my name is", name, "my age is", age, "my address is", address, "my height is", height, "my weight is", weight)
+
+# string = ტექსტური მონაცემთა ტიპი "leqso"
+print("leqso")
+# integer = რიცხვური მონაცემთა ტიპი 15
+print(15)
+# float = ათწილადები 12.5
+print(12.5)
+
+# დეკლარაცია არის ცვლადის სახელის შექმნა name
+# ინიციალიზება არის ცვლადისთვის მნიშვნელობის მინიჭება name = "leqso"
+# რეინიციალიზება არის ცვლადის სახელის განახლება ან შეცვლა name = "luka"
