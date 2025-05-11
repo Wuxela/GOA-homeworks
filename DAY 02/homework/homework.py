25df34fe16d84cf3e637137c2dc8bb0850d19eb0
Wuxela <wuxela7@gmail.com>
2025-05-11 14:42:52 +0400

    commit

---
+++ DAY 02/homework/homework.py
@@ -0,0 +1,20 @@
+number1 = 15
+number2 = 20
+print(number1 + number2)   
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+
+
+birth_year = 2010
+current_year = 2025
+age = current_year - birth_year
+
+
+number1 = 12
+number2 = 39
+number3 = 61
+number4 = 3
+number5 = 99  
+print((number1 + number2 + number3 + number4 + number5) / 5)
