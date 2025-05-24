006e13bec1cd2635764661d58bb2bd8e0c889cc3
Wuxela <wuxela7@gmail.com>
2025-05-24 23:10:14 +0400

    commit

---
+++ DAY 04/homework/task 02/homework2.py
@@ -0,0 +1,31 @@
+name = "asia"
+age = 44
+print(age , name)
+
+# შექმენით ორი float ტიპის ცვლადი
+length = 5.5  # სიგრძე
+width = 3.2   # სიგანე
+
+# გამოთვალეთ მართკუთხედის ფართობი
+area = length * width
+
+# დაბეჭდეთ შედეგი,
+print("მართკუთხედის ფართობია area")
+
+
+# შექმენით str ტიპის ცვლადი ნივთის სახელისთვის და float ტიპის ცვლადი ფასისთვის
+item_name = "notebook"  # ნივთის სახელი
+item_price = 2599.99   # ნივთის ფასი
+
+# დაბეჭდეთ წინადადება, რომელიც აერთიანებს ორივეს
+print(f"item: {item_name}, price: {item_price} lari.")
+
+
+age = 14
+age = 14
+
+print(age + age)
+
+item_price = 200 
+item_price1 = 2
+print(item_price / item_price1)
