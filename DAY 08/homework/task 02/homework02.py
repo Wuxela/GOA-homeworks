696c0113bb3aed29998b573046529326ad107c13
Wuxela <wuxela7@gmail.com>
2025-06-21 19:03:18 +0400

    commit

---
+++ DAY 08/homework/task 02/homework02.py
@@ -0,0 +1,11 @@
+name = "leqso"
+last_name = "termakozashvlili"
+age = 96
+address = "gori"
+height = 1.78
+
+print(type(name))
+print(type(last_name))
+print(type(age))
+print(type(address))
+print(type(height))
