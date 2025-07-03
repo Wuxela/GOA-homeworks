a7050f157f4022f671931e5809616f325dd61eea
Wuxela <wuxela7@gmail.com>
2025-07-03 16:13:24 +0400

    commit

---
+++ DAY 10/homework/homework04.py
@@ -0,0 +1,24 @@
+# მაგალითი_1
+price = 9.99
+price_int = int(price)
+print(price_int) 
+
+# მაგალითი_2
+num_str = "25"
+num_int = int(num_str)
+print(num_int) 
+
+# მაგალითი_3
+age = 2.1
+age_str = str(age)
+print(age_str)
+
+# მაგალითი_4
+num_str = "3.14"
+num_float = float(num_str)
+print(num_float)
+
+# მაგალითი_5
+age_str = "18"
+age_int = int(age_str) 
+print(age_int)
---
+++ DAY 10/homework/homework07.py
@@ -0,0 +1,7 @@
+name = 'Leqso'
+last_name = 'Termakozashvili'
+age = 15
+height = 1.76
+address = 'GORI'
+
+print("I'm " + name + " " + last_name + " I am " + str(age) + " years old my height is " + str(height) + "m I live in " + address)
