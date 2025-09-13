0864ab0c0e3765a5de2211c6f33417340518472e
Wuxela <wuxela7@gmail.com>
2025-09-13 14:00:50 +0400

    commit

---
+++ DAY 20/classwork/classwork.py
@@ -0,0 +1,8 @@
+for i in range(20):
+    print('leqso termakozashvili')
+
+for i in range(5 , 40):
+    print(i)
+
+for i in range(50 , 100 , 5):
+    print(i)
---
+++ DAY 20/homework/homework4.py
@@ -0,0 +1,4 @@
+i=1
+while i < 6:
+    print("leqso")
+    i = i + 1
