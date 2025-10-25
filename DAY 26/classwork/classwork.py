02d0fdb72926ef45e82dcb3e027dfba5ace6304c
Wuxela <wuxela7@gmail.com>
2025-10-25 14:08:34 +0400

    commit

---
+++ DAY 26/classwork/classwork.py
@@ -0,0 +1,11 @@
+items = ["რაღაცა1", "რაღაცა2", "რაღაცა3", "რაღაცა4", "რაღაცა5", "რაღაცა6", "რაღაცა7"]
+
+first_part = items[:3]
+
+middle_part = items[2:5]
+
+last_part = items[3:]
+
+print("ნაწილი 1:", first_part)
+print("ნაწილი 2:", middle_part)
+print("ნაწილი 3:", last_part)
