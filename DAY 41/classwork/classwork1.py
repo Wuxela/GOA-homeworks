fc77400bfa1c6dbc1b02b58d6909383f3954f997
Wuxela <wuxela7@gmail.com>
2026-02-10 20:10:59 +0400

    commit

---
+++ DAY 41/classwork/classwork1.py
@@ -0,0 +1,7 @@
+def cookie(x):
+    if isinstance(x, str):
+        return "Who ate the last cookie? It was Zach!"
+    elif isinstance(x, (int, float)):
+        return "Who ate the last cookie? It was Monica!"
+    else:
+        return "Who ate the last cookie? It was the dog!"
